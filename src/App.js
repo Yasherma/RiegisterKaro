19f3ae332d8db5e463a3f6e458e2d20958e4f5e5
Yashverma <yv3245650@gmail.comgit config --global user.name YashVerma>
2025-01-20 02:16:20 +0530

    registerKaro

---
+++ src/App.js
@@ -0,0 +1,355 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div id='cantain' >
+      {/* <h1>hello | YASH VERMA</h1> */}
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      {/* <div id="root"></div> */}
+      <div id="head">
+        <h4>
+          <a href="#">
+            <i className="fa-regular fa-envelope"></i>www.registerKaro.in |
+          </a>
+          <a href="#">
+            <i className="fa-solid fa-phone-flip"></i>+918458304989 |
+          </a>
+          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-twitter"></i>
+          <i className="fa-solid fa-p"></i>
+        </h4>
+      </div>
+      <br />
+      <div id="head2">
+        <h4 id="Sehed">
+          <label id="big">Register</label>
+          <i id="crown" className="fa-solid fa-crown"></i>
+          <label id="big2">Karo</label>
+          <a id="hed" href="#">
+            Home
+          </a>
+          <label>Our Services</label>
+          <select>
+            <option value=""></option>
+          </select>
+          <a id="hed" href="#">
+            Blog
+          </a>
+          <a id="hed" href="#">
+            Contact Us
+          </a>
+          <a id="hed" href="#">
+            About Us <i className="fa-solid fa-magnifying-glass"></i>
+          </a>
+          <button>Talk An Expert</button>
+        </h4>
+      </div><br />
+      <div id="head3"></div>
+      <div id='head4'>
+        <h1>Trusted By Over 100+ StartUp and Freelance Business<br />
+        <img src='https://logos-world.net/wp-content/uploads/2020/09/Oracle-Logo.png'></img>
+        <img id='ig' src='https://s3.us-west-2.amazonaws.com/greenreedtech.com/moving-to-morpheus-data/Morpheus-horizontal-v2.png'></img>
+       <img id='ig' src='https://s3.us-west-2.amazonaws.com/greenreedtech.com/moving-to-morpheus-data/Morpheus-horizontal-v2.png'></img>
+        <img src='https://logos-world.net/wp-content/uploads/2020/04/Samsung-Symbol.png'></img> 
+        <img id='ig' src='https://monday.com/p/wp-content/uploads/2023/03/Logo-monday.com-2.png'></img>  
+       <img src='https://www.cdnlogo.com/logos/s/30/segment.svg'></img>
+
+        </h1>
+      </div>
+      <div id='head5'><br/>
+        WELCOME TO REGISETERKARO.IN<br />
+       <b> Explore My Services</b><br/><br/>
+       <div id='box1' ><i class="fa-solid fa-table"></i><i class="fa-solid fa-gears"></i><br/>
+        Company Formation <br/>Build web-based solutions that
+        enhance customer experience. <br/><br/><br/><br/><br/><br/>
+        Learn more 
+</div>
+
+       <div id='box1'><i class="fa-solid fa-building"></i><i class="fa-solid fa-magnifying-glass-plus"></i><br/>
+        Company Secretarial Services <br/>Make data-driven decisions and
+utilize technology to reach
+business goals.<br/><br/><br/><br/><br/><br/> Learn more 
+</div>
+
+       <div id='box1'><i class="fa-solid fa-users-gear"></i><br/>
+        Virtual Office Address <br/>Foster customer relationships by
+        effectively serving your market.<br/><br/><br/><br/><br/><br/> Learn more 
+        </div><br/><br/>
+
+       <div id='box1'><i class="fa-solid fa-sitemap"></i><br/>
+        Annual Compliance Services <br/>Turn your ideas into modern
+        products with our design experts.<br/><br/><br/><br/><br/><br/> Learn more </div>
+
+       <div id='box1'><i class="fa-solid fa-earth-asia"></i><i class="fa-solid fa-cart-shopping"></i><br/>
+        Payroll Services <br/>Expand your business across the
+        globe with minimal effort.<br/><br/><br/><br/><br/><br/> Learn more </div>
+
+       <div id='box1'><i class="fa-solid fa-book-atlas"></i><i class="fa-solid fa-bullhorn"></i><br/>
+        Bookkeeping Services <br/>Steering user behaviours with
+creative design, data insights &
+technology.<br/><br/><br/><br/><br/><br/> Learn more </div>
+      </div>
+      <div id='head6'>
+       
+   <p id='Wel'>  WELCOME TO REGISTERKARO.IN </p><br/>
+   <b id='Ab'> About </b><b id='Reg'>Register Karo</b> <br/>
+We have been using Intelegencia as<br/> our DevOps vendor for our field service<br/> applications over the last couple of years and I’m extremely pleased with<br/>
+their performance, ability to execute, and willingness to adapt in our ever changing<br/> environment. Perry is an outstanding leader who is fanatical about<br/>
+customer satisfaction. He has built a solid team which has consistently delivered<br/> on projects thereby exceeding everyone’s expectations. <br/><br/><br/>
+
+I would strongly recommend their services to any organization that is looking for <br/>solid, reliable, and predictable outcomes.<br/>
+<button id='Learn'>Learn More</button>
+<img id='group' src='https://media.licdn.com/dms/image/v2/D5622AQFsw02PqCrSuQ/feedshare-shrink_800/feedshare-shrink_800/0/1727798904246?e=2147483647&v=beta&t=8yBR_XgQBzAFgj7qEvSlXLHD_XzVHkf6IVbEU91IbFU'></img>
+
+      </div>
+      <div id='head7'><br/><br/>
+     <p id='hed7'>WHY REGISTERKARO.IN  </p><br/>
+ <p> <b  id="h07">Why Choose Register Karo</b> <br/>
+It is with consistent services and results that build trust with the people and that in<br/>
+turn help us to serve the business better</p>.<br/>
+
+
+<div id='box2'>Confidential & Safe<br/>
+All your private information is safe
+with us</div>
+<div id='box3'>No Hidden Fee<br/>
+Everything is put before you with<br/>
+no hidden charges or conditions</div><br/>
+
+<div id='box4'>Guaranteed Satisfaction<br/>
+We ensure that you stay 100%<br/>
+satisfied with our offered services</div>
+
+<div id='box5'>Expert CA/CS Assistance<br/>
+Prompt support from our in-house<br/>
+expert professionals</div>
+
+<div id='box6'> Confidential & Safe<br/>
+All your private information is safe<br/>
+with us</div>
+         
+      </div><br/><br/>
+      <div id='head8'><br/>
+    
+     <b> 0ur Video Introductions </b><br/>
+   <p id='pra'>Velit purus egestas tellus phasellus. Mattis eget sed faucibus magna <br/>vulputate pellentesque a diam tincidunt apis dui.</p><br/>
+ <b><i class="fa-solid fa-lightbulb"></i>Explore ideas together</b><br/>
+ <p id='pra'>Engage audience segments and finally create actionable insights.<br/> Amplify vertical integration.</p><br/>
+ <b><i class="fa-solid fa-tv"></i>Bring those ideas to life</b>
+ <p id='pra'>Engage audience segments and finally create actionable insights.<br/> Amplify vertical integration.<br/></p>
+         
+ <video>
+  <source></source>
+ </video>
+      </div>
+<div id='head9'>
+  <b id='hed9'>Our Happy Clients</b>
+  <p id='hed01'>Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource<br/>-leveling customer service for state of the art customer service.</p>
+<a id="b1" href="https://goggle.com"></a>
+<a id="b2" href="https://goggle.com"></a>
+<a id="b3" href="https://goggle.com"></a>
+<a id="b4" href="https://goggle.com"></a>
+<a id="b5" href="https://goggle.com"></a>
+<a id="b6" href="https://goggle.com"></a>
+<a id="b7" href="https://goggle.com"></a>
+<a id="b8" href="https://goggle.com"></a>
+<a id="b9" href="https://goggle.com"></a>
+<a id="b10" href="https://goggle.com"></a>
+<a id="b11"href="https://goggle.com"></a>
+
+</div>
+<div id="head10">
+  <br/>
+  <a id="hed11" href="#">Fill up Application Form</a>
+  <i id="hed1" class="fa-regular fa-newspaper"></i>
+
+  <a id="hed11" href="#">Make Online Payment</a>
+  <i id="hed1" class="fa-solid fa-envelopes-bulk"></i>
+
+  <a id="hed11" href="#">Executive will Process Application</a>
+  <i id="hed1" class="fa-solid fa-user-astronaut"></i>
+
+  <a id="hed11" href="#">Get Confirm Mail</a>
+  <i id="hed1" class="fa-solid fa-envelope-open-text"></i>
+
+</div>
+<div id="head11"><br/>
+EXPLORE OUR BLOGS<br/>
+ <b id="h11">Accelerate Digital Transformation </b>
+ <br/>
+ <div id="b01"><img id="im01" src="https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333_1280.jpg"></img>
+ Prabhash Mishra • 1 Jan 2023 • Today<br/>
+Small business & Startup <br/>
+Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty? <br/>
+<p id="P1">Tax & Audit <br/>
+Management <br/></p>
+ </div>
+ <div id="b01"><img id="im01" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEhGrSrLAh9uHNk5k9MZ_XmbrJUq69DjGwAL9C304Zr8vOWrN-KHnwBbfQ1IUmI22shQ&usqp=CAU"></img>
+ Mahesh Kumar • 1 Jan 2023<br/>
+Scale-Up Company Offer<br/>
+Mental models are simple expressions of complex processes or relationships.<br/>
+<p id="P1">Tax<br/>
+Research<br/>
+Complience<br/></p>
+ </div>
+ <div id="b01"><img id="im01" src="https://writersvictoria.org.au/wp-content/uploads/2022/02/BusinessWriting.png"></img>
+ Rakhi Verma • 1 Jan 2023<br/>
+Growing Business Package<br/>
+Introduction to Wireframing and its Principles. Learn from the best in the industry.<br/>
+<p id="P1">Audit<br/>
+Money Back<br/></p>
+ </div><br/>
+ <div id="b01"><img id="im01" src="https://c8.alamy.com/comp/B8RWEY/boy-pasting-a-poster-on-a-wall-in-lijiang-yunnan-province-china-B8RWEY.jpg"></img>
+ Karan Kumar • 1 Jan 2023<br/>
+Scale-Up Company Offer<br/>
+Collaboration can make our teams stronger, and our individual designs better.<br/>
+<p id="P1">Money<br/>
+Management<br/>
+Presentation<br/></p>
+ </div>
+ <div id="b01"><img id="im01" src="https://img.freepik.com/premium-photo/man-sitting-desk-front-computer-generative-ai-image_87646-18311.jpg"></img>
+ Richa Singh • 1 Jan 2023<br/>
+Scale-Up Company Offer<br/>
+JavaScript frameworks make development easy with extensive features and functionalities.<br/>
+<p id="P1">Tax Return<br/>
+News<br/>
+Audit<br/></p>
+ </div>
+ <div id="b01"><img id="im01" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQZT5xkP7SahBqqasFEhkXtKcSHfPrg-2WvjaIk5yUyEEfqQS0NKVcg-V0YjywUAXsdWo&usqp=CAU"></img>
+ Miss Nora• 1 Jan 2023<br/>
+Scale-Up Company Offer<br/>
+Starting a community doesn’t need to be complicated, but how do you get started?<br/>
+<p id="P1">Private Limited Company<br/>
+Customer Success</p><br/>
+
+<br/>
+ </div><br/><br/>
+ <button id="btn11">Show more blogs-<i class="fa-solid fa-chevron-right"></i></button>
+ </div>
+ 
+  <div id="head12"><br/>
+  <h3>What peoples says about us </h3><br/>
+  <div id="d01">
+    <img id="m2" src="https://static.vecteezy.com/system/resources/previews/020/539/492/non_2x/five-stars-rating-button-yellow-rating-stars-on-white-background-free-vector.jpg"></img>
+  <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco<br/> 
+  laboris nisi ut aliquip ex ea commodo consequat aute irure sint<br/> amet occaecat cupidatat non proident ea commodo consequat <br/> 
+  aute irure sint amet occaecat cupidatat non proident
+  <img id="m3"src="https://i.pinimg.com/736x/3d/1c/c3/3d1cc31d8a2a1489052408d8bad40177.jpg"></img>
+   </div>
+  <div id="d01"><br/><img id="m2" src="https://static.vecteezy.com/system/resources/previews/020/539/492/non_2x/five-stars-rating-button-yellow-rating-stars-on-white-background-free-vector.jpg"></img>
+  <br/>Ut enim ad minim veniam, quis nostrud exercitation<br/>
+   ullamco laboris nisi ut aliquip ex ea commodo consequat<br/>
+   aute irure sint amet occaecat cupidatat non proident ea commodo consequat <br/>
+   aute irure sint amet occaecat cupidatat non proident 
+
+<img id="m3"src="https://i.pinimg.com/736x/3d/1c/c3/3d1cc31d8a2a1489052408d8bad40177.jpg"></img>
+  </div>
+  <div id="d01"><br/><img id="m2" src="https://static.vecteezy.com/system/resources/previews/020/539/492/non_2x/five-stars-rating-button-yellow-rating-stars-on-white-background-free-vector.jpg"></img>
+  <br/>
+  Ut enim ad minim veniam, quis nostrud exercitation <br/>
+  ullamco laboris nisi ut aliquip ex ea commodo consequat aute irure<br/>
+   sint amet occaecat cupidatat non proident ea commodo consequat aute irure sint<br/>
+    amet occaecat cupidatat non proident <br/>
+    <img id="m3"src="https://i.pinimg.com/736x/3d/1c/c3/3d1cc31d8a2a1489052408d8bad40177.jpg"></img>
+
+
+  </div>
+ 
+</div>
+<div id="head13"><br/>
+  FAQ
+  <h3 id="h13">Frequent Ask Questions </h3><br/>
+  <div id="S13">Can I recover deleted files from desktop with this software?<i id="s03" class="fa-solid fa-chevron-right"></i></div><br/>
+  <div id="S13">Can I recover deleted files from desktop with this software?<i id="s03" class="fa-solid fa-chevron-right"></i></div><br/>
+  <div id="S13">Can I recover deleted files from desktop with this software?<i id="s03" class="fa-solid fa-chevron-right"></i></div><br/>
+  <div id="S13">Can I recover deleted files from desktop with this software?<i id="s03" class="fa-solid fa-chevron-right"></i></div><br/>
+  <div id="S13">Can I recover deleted files from desktop with this software?<i  id="s03" class="fa-solid fa-chevron-right"></i></div><br/>
+  <button id="btn11">Show more <i class="fa-solid fa-chevron-right"></i>-</button>
+</div>
+<div id="head14"><br/>
+<b id="hed14">Manage Your Services by your<br/>
+      Mobile Phone<br/></b>
+<p id="p14">Download our app to manage and track your services. Our app<br/> enables you to stay in touch<br/>
+ with our experts and aids you in tracking your progress. </p>
+ Get the App
+<img id="I14" src='https://www.registerkaro.in/_next/static/media/Get_Online_EPF_Registration_in_India_certificate.8c84f153.svg'></img>
+</div>
+<div id="head15"><br/>
+1% OF THE INDUSTRY<br/>
+<b>Welcome to your new digital reality. Now.</b><br/><br/><br/><br/>
+<input 
+        type="search" 
+        id="searchInput" 
+        placeholder="Enter your Email" 
+      />
+      <button >Submit</button><br/><br/><br/>
+      <i class="fa-solid fa-circle-check"></i>  Instant results 
+       <i class="fa-solid fa-circle-check"></i> User-friendly interfacem  
+       <i class="fa-solid fa-circle-check"></i>  Personalized customization
+</div>
+<div id='head16'>
+<img id='h16'
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/2560px-Spotify_logo_with_text.svg.png" 
+        alt="Example Image"
+      />
+<img  id='h16'
+       src='https://www.investopedia.com/thmb/5vxm2WD25BX7DwA00POoSvVSrFw=/fit-in/1500x750/filters:format(png):fill(white):max_bytes(150000):strip_icc()/Coinbase_Wordmark-719a982e55e945fc93a3b1770b6a9aa9.jpg'
+
+ alt="Example Image"
+
+      />
+      <img id='h16'
+
+
+
+src='https://miro.medium.com/v2/resize:fit:1400/1*Rj8MpTHHRdR7Bu5Gzd82hw.png'
+      />
+
+      <img  id='h16'
+      src='https://i.pcmag.com/imagery/reviews/01eqrJlwTzs4ACRmWo01fCt-45..v1668723864.jpg' 
+      
+      />
+      <img id='h16'
+      src='https://cdn.prod.website-files.com/669eb1086f08fffc3efda56e/66f31eb396391f8b83ec460c_Webflow_logo_2023.svg.png'
+      />
+      <img id='h16'
+      src='https://infocanvas.upenn.edu/wp-content/uploads/2024/08/Zoom-Logo.png'
+      />
+</div>
+
+<div id='head17'><br/><br/>
+Design outstanding interfaces <br/> with advanced Figma features<br/> in a matter of minutes.<br/>
+<i class="fa-brands fa-instagram"></i><i class="fa-brands fa-google"></i><i class="fa-brands fa-apple"></i><i class="fa-brands fa-facebook"></i>
+<h5 id='hed15'>START A BUSINESS</h5>
+<p id='p17'>Features<br/>
+Solutions<br/>
+Integrations<br/>
+Enterprise<br/>
+Solutions</p><br/>
+
+<p id='pi17'>GOVERNMENT REGISTRATION<br/>
+Partners<br/>
+Community<br/>
+Developers<br/>
+App<br/>
+Blog</p>
+<p id='pi01'> COMPLIANCE & TAX<br/>
+Channels<br/>
+Scale<br/>
+Watch the Demo<br/>
+Our Competition</p>
+<br/>
+<i id='foot' class="fa-solid fa-arrow-up-from-bracket"></i>
+© 2024 Registerkaro. All Rights Reserved.
+</div>
+      </div>
+    
+  );
+}
+
+export default App;
+
+
